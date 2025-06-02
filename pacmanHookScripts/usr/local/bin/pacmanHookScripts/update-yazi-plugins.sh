f5d7684230c28c4102ae386e6fe1cb0ae7b65c3e
dalailahner <59581086+dalailahner@users.noreply.github.com>
2025-06-02 13:08:00 +0200

    fix deprecated yazi config elements

---
+++ pacmanHookScripts/usr/local/bin/pacmanHookScripts/update-yazi-plugins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for dir in /home/*; do
+  user=$(basename "$dir")
+  # check if real user and if proper directory
+  if id "$user" &>/dev/null && [ -d "$dir" ]; then
+    echo "updating $user's yazi plugins"
+    sudo -u "$user" /usr/bin/ya pkg --upgrade
+  fi
+done
