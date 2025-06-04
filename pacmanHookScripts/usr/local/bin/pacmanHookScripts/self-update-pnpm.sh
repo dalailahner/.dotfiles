dcb90bde763ccf6483783f6a288258d741d48a2e
dalailahner <59581086+dalailahner@users.noreply.github.com>
2025-06-05 00:07:17 +0200

    add pnpm self-update hook

---
+++ pacmanHookScripts/usr/local/bin/pacmanHookScripts/self-update-pnpm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for dir in /home/*; do
+  user=$(basename "$dir")
+  # check if real user and if proper directory
+  if id "$user" &>/dev/null && [ -d "$dir" ]; then
+    echo "self-update $user's pnpm"
+    sudo -u "$user" "/home/$user/.local/share/pnpm/pnpm" self-update
+  fi
+done
