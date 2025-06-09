73fb25d69ee2258c4b95ab9c774215c0ca8f6ee0
dalailahner <59581086+dalailahner@users.noreply.github.com>
2025-06-09 20:46:22 +0200

    move scripts to dedicated folder

---
+++ scripts/backupToNasScript.sh
@@ -0,0 +1,86 @@
+#! /bin/bash
+
+### IMPORTANT:
+### $backupMachineIP & $MEDIA_DRIVE_UUID are an environment variables that must be set in /etc/profile or /etc/environment
+
+errorLogFile="$HOME/backupErrorLog.txt"
+echo -n "ssh username: "
+read -r SSHusername
+
+if [[ -z "$backupMachineIP" ]]; then
+	echo "environment variable backupMachineIP is not set. please set it in /etc/profile or /etc/environment and reboot"
+	exit 1
+fi
+
+if [[ -z "$MEDIA_DRIVE_UUID" ]]; then
+	echo "environment variable MEDIA_DRIVE_UUID is not set. please set it in /etc/profile or /etc/environment and reboot"
+	exit 1
+fi
+
+rm -f "$errorLogFile"
+date > "$errorLogFile"
+
+homeDirBackup () {
+	if [[ "$#" == 1 && -n "$1" ]]; then
+		sudo -u "$1" rsync -aAXv --delete --exclude "/home/$1/Documents/" --exclude "/home/$1/Pictures/" --exclude "/home/$1/Videos/" --exclude "/home/$1/Music/" --exclude "/home/$1/3D/" --exclude "/home/$1/.cache/" --exclude "/home/$1/.local/share/Trash/" --exclude "/home/$1/.local/share/Steam/" --exclude "/home/$1/.local/share/pnpm/store/" --exclude "/home/$1/.var/" /home "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/archlinux/" 2>> "$errorLogFile"
+		sudo -u "$1" rsync -aAXv --delete "/home/$1/Documents" "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/media/" 2>> "$errorLogFile"
+		picFolder=$(readlink -f "/home/$1/Pictures")
+		sudo -u "$1" rsync -aAXv --delete "$picFolder" "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/media/" 2>> "$errorLogFile"
+		vidFolder=$(readlink -f "/home/$1/Videos")
+		sudo -u "$1" rsync -aAXv --delete "$vidFolder" "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/media/" 2>> "$errorLogFile"
+		sudo -u "$1" rsync -aAXv --delete "/home/$1/Music" "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/media/" 2>> "$errorLogFile"
+		sudo -u "$1" rsync -aAXv --delete "/home/$1/3D" "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/media/" 2>> "$errorLogFile"
+	else
+		echo "homeDirBackup function needs exactly 1 argument with a non-empty string (got $#)."
+	fi
+}
+
+echo "checking connection to the backup machine..."
+if ping -q -4 -c 3 "$backupMachineIP" &> /dev/null; then
+
+	echo "init backup to $backupMachineIP..."
+
+	for dir in /home/*; do
+		user=$(basename "$dir")
+		# check if real user and if proper directory
+		if id "$user" &> /dev/null && [ -d "$dir" ]; then
+
+			echo "checking if $user's media drive ($MEDIA_DRIVE_UUID) is mounted..."
+			if mountpoint "/run/media/$user/$MEDIA_DRIVE_UUID" &> /dev/null; then
+
+				echo "media drive is mounted, backing up $user's home directory..."
+				homeDirBackup "$user"
+
+			else
+				echo "media drive is NOT mounted. trying to mount drive..."
+				sudo mkdir -p "/run/media/$user/$MEDIA_DRIVE_UUID"
+				sudo mount -t ntfs-3g -o uid=1000,gid=1000 --uuid "$MEDIA_DRIVE_UUID" "/run/media/$user/$MEDIA_DRIVE_UUID"
+
+				if mountpoint "/run/media/$user/$MEDIA_DRIVE_UUID" &> /dev/null; then
+					echo "media drive is mounted, backing up $user's home directory..."
+					homeDirBackup "$user"
+
+				else
+					echo "couldn't mount $user's media drive. skipping backup of $user's home directory..."
+				fi
+
+				sudo umount "/run/media/$user/$MEDIA_DRIVE_UUID"
+				if sudo echo $?; then
+					sudo rm -rf "/run/media/$user/$MEDIA_DRIVE_UUID"
+				fi
+			fi
+		fi
+	done
+
+	echo "backing up system files..."
+	sudo rsync -aAXv --delete /root "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/archlinux/" 2>> "$errorLogFile"
+	sudo rsync -aAXv --delete /etc "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/archlinux/" 2>> "$errorLogFile"
+	sudo rsync -aAXv --delete /boot "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/archlinux/" 2>> "$errorLogFile"
+	sudo rsync -aAXv --delete /usr/local "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/archlinux/usr/" 2>> "$errorLogFile"
+
+	exit 0
+
+else
+	echo "$backupMachineIP not reachable"
+	exit 1
+fi
