eaddf59432732cfe84b071d34aea2d59152f9cfe
dalailahner <59581086+dalailahner@users.noreply.github.com>
2025-08-09 00:38:01 +0200

    prettier feedback msgs

--- scripts/backupToNasScript.sh
+++ scripts/backupToNasScript.sh
@@ -8,12 +8,12 @@ echo -n "ssh username: "
 read -r SSHusername
 
 if [[ -z "$backupMachineIP" ]]; then
-	echo "environment variable backupMachineIP is not set. please set it in /etc/profile or /etc/environment and reboot"
+	printf "environment variable backupMachineIP is not set. please set it in /etc/profile or /etc/environment and reboot\n"
 	exit 1
 fi
 
 if [[ -z "$MEDIA_DRIVE_UUID" ]]; then
-	echo "environment variable MEDIA_DRIVE_UUID is not set. please set it in /etc/profile or /etc/environment and reboot"
+	printf "environment variable MEDIA_DRIVE_UUID is not set. please set it in /etc/profile or /etc/environment and reboot\n"
 	exit 1
 fi
 
@@ -22,56 +22,64 @@ date > "$errorLogFile"
 
 homeDirBackup () {
 	if [[ "$#" == 1 && -n "$1" ]]; then
-		excludePaths=("Documents" "Pictures" "Videos" "Music" "3D" ".cache" ".local/share/Trash" ".var" ".local/share/klipper" ".local/share/Steam" ".local/share/pnpm/store" ".wine/drive_c/users/dalailahner/AppData/Local/Temp" ".config/vesktop/sessionData")
+	  printf "media drive is mounted\n"
+	  printf "\n"
+	  echo "---------------------------------------------------------------------------------"
+	  printf "\nbacking up $1's home directory...\n"
+		excludePaths=("Documents" "Pictures" "Videos" "Music" "3D" ".cache" ".local/share/Trash" ".var" ".local/share/klipper" ".local/share/Steam" ".local/share/pnpm/store" ".wine/drive_c/users/dalailahner/AppData/Local/Temp" ".config/vesktop/sessionData" ".config/micro/buffers")
     excludeListArr=()
     for folder in "${excludePaths[@]}"; do
         excludeListArr+=(--exclude="/home/$1/$folder")
     done
     sudo -u "$1" rsync -aAXv --delete "${excludeListArr[@]}" /home "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/archlinux/" 2>> "$errorLogFile"
 
+		printf "\nbacking up Documents ...\n"
 		sudo -u "$1" rsync -aAXv --delete "/home/$1/Documents" "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/media/" 2>> "$errorLogFile"
 
+		printf "\nbacking up Pictures ...\n"
 		picFolder=$(readlink -f "/home/$1/Pictures")
 		sudo -u "$1" rsync -aAXv --delete "$picFolder" "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/media/" 2>> "$errorLogFile"
 
+		printf "\nbacking up Videos ...\n"
 		vidFolder=$(readlink -f "/home/$1/Videos")
 		sudo -u "$1" rsync -aAXv --delete "$vidFolder" "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/media/" 2>> "$errorLogFile"
 
+		printf "\nbacking up Music ...\n"
 		sudo -u "$1" rsync -aAXv --delete "/home/$1/Music" "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/media/" 2>> "$errorLogFile"
 
+		printf "\nbacking up 3D ...\n"
 		sudo -u "$1" rsync -aAXv --delete "/home/$1/3D" "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/media/" 2>> "$errorLogFile"
 	else
-		echo "homeDirBackup function needs exactly 1 argument with a non-empty string (got $#)."
+		printf "homeDirBackup function needs exactly 1 argument with a non-empty string (got $#).\n"
 	fi
 }
 
-echo "checking connection to the backup machine..."
+printf "checking connection to the backup machine...\n"
 if ping -q -4 -c 3 "$backupMachineIP" &> /dev/null; then
 
-	echo "init backup to $backupMachineIP..."
+	printf "init backup to $backupMachineIP...\n"
 
 	for dir in /home/*; do
 		user=$(basename "$dir")
 		# check if real user and if proper directory
 		if id "$user" &> /dev/null && [ -d "$dir" ]; then
 
-			echo "checking if $user's media drive ($MEDIA_DRIVE_UUID) is mounted..."
+			printf "checking if $user's media drive ($MEDIA_DRIVE_UUID) is mounted...\n"
 			if mountpoint "/run/media/$user/$MEDIA_DRIVE_UUID" &> /dev/null; then
 
-				echo "media drive is mounted, backing up $user's home directory..."
 				homeDirBackup "$user"
 
 			else
-				echo "media drive is NOT mounted. trying to mount drive..."
+				printf "media drive is NOT mounted. trying to mount drive...\n"
 				sudo mkdir -p "/run/media/$user/$MEDIA_DRIVE_UUID"
 				sudo mount -t ntfs-3g -o uid=1000,gid=1000 --uuid "$MEDIA_DRIVE_UUID" "/run/media/$user/$MEDIA_DRIVE_UUID"
 
 				if mountpoint "/run/media/$user/$MEDIA_DRIVE_UUID" &> /dev/null; then
-					echo "media drive is mounted, backing up $user's home directory..."
+				
 					homeDirBackup "$user"
 
 				else
-					echo "couldn't mount $user's media drive. skipping backup of $user's home directory..."
+					printf "couldn't mount $user's media drive. skipping backup of $user's home directory...\n"
 				fi
 
 				if sudo umount "/run/media/$user/$MEDIA_DRIVE_UUID"; then
@@ -83,21 +91,29 @@ if ping -q -4 -c 3 "$backupMachineIP" &> /dev/null; then
 		fi
 	done
 
-	echo "backing up /root ..."
+	printf "\n"
+	echo "---------------------------------------------------------------------------------"
+	printf "\nbacking up /root ...\n"
 	sudo rsync -aAXv --delete --exclude="/root/.cache" /root "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/archlinux/" 2>> "$errorLogFile"
 
-	echo "backing up /etc ..."
+	printf "\n"
+	echo "---------------------------------------------------------------------------------"
+	printf "\nbacking up /etc ...\n"
 	sudo rsync -aAXv --delete /etc "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/archlinux/" 2>> "$errorLogFile"
 
-	echo "backing up /boot ..."
+	printf "\n"
+	echo "---------------------------------------------------------------------------------"
+	printf "\nbacking up /boot ...\n"
 	sudo rsync -aAXv --delete /boot "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/archlinux/" 2>> "$errorLogFile"
 
-	echo "backing up /usr/local ..."
+	printf "\n"
+	echo "---------------------------------------------------------------------------------"
+	printf "\nbacking up /usr/local ...\n"
 	sudo rsync -aAXv --delete /usr/local "$SSHusername@$backupMachineIP:/mnt/MAIN/NAS/BACKUPS/archlinux/usr/" 2>> "$errorLogFile"
 
 	exit 0
 
 else
-	echo "$backupMachineIP not reachable"
+	printf "$backupMachineIP not reachable\n"
 	exit 1
 fi
