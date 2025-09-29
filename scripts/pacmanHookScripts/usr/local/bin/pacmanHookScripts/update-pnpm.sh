90f54bcdf56830c634e61c7c68bece0e83839754
dalailahner <59581086+dalailahner@users.noreply.github.com>
2025-09-29 08:27:47 +0200

    fix script

--- scripts/pacmanHookScripts/usr/local/bin/pacmanHookScripts/update-pnpm.sh
+++ scripts/pacmanHookScripts/usr/local/bin/pacmanHookScripts/update-pnpm.sh
@@ -4,6 +4,7 @@ for dir in /home/*; do
   # check if real user and if proper directory
   if id "$user" &>/dev/null && [ -d "$dir" ]; then
     echo "updating $user's pnpm"
-    "/home/$user/.local/share/pnpm/pnpm" self-update
+    cd "/home/$user/.local/share/pnpm"
+    ./pnpm self-update
   fi
 done
