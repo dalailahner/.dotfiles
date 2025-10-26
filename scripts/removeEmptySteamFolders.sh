cc02ed1de227553b0e34e3cc711c4c46e186ec36
dalailahner <59581086+dalailahner@users.noreply.github.com>
2025-10-26 20:35:01 +0100

    add steam folders from firejail.profile

--- scripts/removeEmptySteamFolders.sh
+++ scripts/removeEmptySteamFolders.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-FOLDERS=(.factorio .killingfloor .klei .mbwarband .paradoxinteractive .paradoxlauncher .prey Zomboid)
+
+FOLDERS=(.config/Epic .config/Loop_Hero .config/MangoHud .config/ModTheSpire .config/RogueLegacy .config/UNDERTALE .config/unity3d .factorio .killingfloor .klei .mbwarband .paradoxinteractive .paradoxlauncher .prey Zomboid .local/share/3909 .local/share/aspyr-media .local/share/Baba_Is_You .local/share/bohemiainteractive .local/share/cdprojektred '.local/share/Colossal Order' .local/share/Dredmor .local/share/FasterThanLight .local/share/feral-interactive .local/share/HotlineMiami .local/share/IntoTheBreach '.local/share/Paradox Interactive' .local/share/PillarsOfEternity .local/share/RogueLegacy .local/share/SteamWorldDig '.local/share/SteamWorld Dig 2' .local/share/SuperHexagon .local/share/Terraria .local/share/vpltd)
 
 for FOLDER in "${FOLDERS[@]}"; do
   if [ -d "$HOME/$FOLDER" ]; then
