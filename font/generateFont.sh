446410bde63d3370b3fef62c7a1515d17d4ebded
dalailahner <59581086+dalailahner@users.noreply.github.com>
2026-08-06 20:13:30 +0200

    font update

--- font/generateFont.sh
+++ font/generateFont.sh
@@ -15,7 +15,7 @@ done
 
 # exit if any are missing
 if [ ${#MISSING_COMMANDS[@]} -ne 0 ]; then
-  echo "Error: The following required commands are not available: ${missing[*]}" >&2
+  echo "Error: The following required commands are not available: ${MISSING_COMMANDS[*]}" >&2
   exit 1
 fi
 
@@ -23,17 +23,27 @@ fi
 SCRIPT_LOCATION=$(dirname "$(readlink -f "$0")")
 
 # download needed files
-curl -L -o "/tmp/HackNerdFont-Regular.ttf" "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf"
-curl -L -o "/tmp/Myna-Regular.ttf" "https://raw.githubusercontent.com/sayyadirfanali/Myna/main/fonts/Myna-Regular.ttf"
+curl -L -o "/tmp/HackNerdFont-Regular.ttf" "https://github.com/ryanoasis/nerd-fonts/raw/refs/heads/master/patched-fonts/Hack/HackNerdFont-Regular.ttf"
+curl -L -o "/tmp/Myna-Regular.ttf" "https://github.com/sayyadirfanali/Myna/raw/refs/heads/main/fonts/Myna-Regular.ttf"
+
+printf "\n"
 
 # run the fontforge script
 /usr/bin/fontforge -script "replaceSymbols.pe" "$SCRIPT_LOCATION/dalailahner-base.sfd" "/tmp/HackNerdFont-Regular.ttf" "/tmp/Myna-Regular.ttf" "$SCRIPT_LOCATION/dalailahner-new.ttf"
+
+fontforgeExitCode=$?
+
+printf "\n"
 
 # cleanup
 rm "/tmp/HackNerdFont-Regular.ttf"
 rm "/tmp/Myna-Regular.ttf"
 
-echo "!!! IMPORTANT !!! rename dalailahner-new.ttf to dalailahner.ttf in order to use updated font. Press Enter to continue..."
-read -r
-
-exit 0
+if [ $fontforgeExitCode -eq 0 ]; then
+  printf "\n!!! IMPORTANT !!! rename dalailahner-new.ttf to dalailahner.ttf in order to use updated font.\n\nPress Enter to continue..."
+  read -r
+  exit 0
+else
+  echo "FontForge exited with error code: $fontforgeExitCode"
+  exit 1
+fi
